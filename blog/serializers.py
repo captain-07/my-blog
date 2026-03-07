1b4e6773db64662cd09f7e80f1f39451705561d9
Debjyoti <sahadebjyoti363@gmail.com>
2026-03-07 22:49:39 +0530

    Adding comments with posts

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,14 +1,19 @@
 from rest_framework import serializers
 from .models import Post, Comment
 
-class PostSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Post
-        fields = "__all__"
 
 class CommentSerializer(serializers.ModelSerializer):
 
+    user = serializers.StringRelatedField(read_only=True)
+
     class Meta:
         model = Comment
+        fields = ["id", "user", "content", "created_at"]
+
+class PostSerializer(serializers.ModelSerializer):
+
+    comments = CommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Post
         fields = "__all__"
