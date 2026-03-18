8aaea92a8cd261c0d83dd0c2d8f7d82135545141
Debjyoti <sahadebjyoti363@gmail.com>
2026-03-18 15:53:57 +0530

    frontend v2

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -5,7 +5,7 @@
  */
 
 // API Configuration
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+const API_BASE_URL = 'https://captain07.pythonanywhere.com/api';
 
 /**
  * Generic API request helper
